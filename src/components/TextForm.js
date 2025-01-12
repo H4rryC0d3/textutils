14e5490f2ca65d849907b30a7bc5ebac3e7bfa69
Harsh Padiyar <158173756+H4rryC0d3@users.noreply.github.com>
2025-01-12 12:12:14 +0530

    Update TextForm.js

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -58,11 +58,11 @@ export default function TextForm(props) {
         color:props.mode==='dark'? 'white':'#042743'}} id="myBox" rows="8"></textarea>
       </div>
 
-      <button className="btn btn-primary mx-1" onClick={handleUpClick}>Convert to Uppercase</button>
-      <button className="btn btn-primary mx-1" onClick={handleLoClick}>Convert to Lovercase</button>
-      <button className="btn btn-primary mx-1" onClick={handleClearClick}>Clear Text</button>
-      <button className="btn btn-primary mx-1" onClick={handleCopy}>Copy Text</button>
-      <button className="btn btn-primary mx-1" onClick={handleExtraSpaces}>Remove Extra Spaces</button>
+      <button className="btn btn-primary mx-1 my-1" onClick={handleUpClick}>Convert to Uppercase</button>
+      <button className="btn btn-primary mx-1 my-1" onClick={handleLoClick}>Convert to Lovercase</button>
+      <button className="btn btn-primary mx-1 my-1" onClick={handleClearClick}>Clear Text</button>
+      <button className="btn btn-primary mx-1 my-1" onClick={handleCopy}>Copy Text</button>
+      <button className="btn btn-primary mx-1 my-1" onClick={handleExtraSpaces}>Remove Extra Spaces</button>
     </div>
 
     <div className="container my-3" style={{color: props.mode==='dark'? 'white':'#042743'}}>
